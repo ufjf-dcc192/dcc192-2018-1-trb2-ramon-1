f834115b31ad94bb7c6d84236f02da9ec0ea4d50
ramonlarivoir <rlarivoir@hotmail.com>
2018-05-30 19:15:14 -0300

    Ajustando validação de novo participante

--- src/java/br/ufjf/dcc192/EventoNovoParticipantePostCommand.java
+++ src/java/br/ufjf/dcc192/EventoNovoParticipantePostCommand.java
@@ -2,6 +2,7 @@ package br.ufjf.dcc192;
 
 import br.ufjf.dcc192.Comando;
 import java.io.IOException;
+import java.util.Date;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -17,8 +18,15 @@ public class EventoNovoParticipantePostCommand implements Comando{
         Long idEvento = Long.parseLong(request.getParameter("idEvento"));
         String nome = request.getParameter("nome");
         String email = request.getParameter("email");
-        EventoDAO.getInstance().novoParticipante(idEvento, nome, email);
-        response.sendRedirect("eventos.html");
+        Evento evento = EventoDAO.getInstance().getEvento(idEvento);
+        Date hoje = new Date();
+        if(evento.getData().after(hoje)) {
+            EventoDAO.getInstance().novoParticipante(idEvento, nome, email);
+            response.sendRedirect("eventos.html");
+        } else {
+            request.setAttribute("titulo", "Inscrições encerradas!");
+            response.sendRedirect("erro.html");
+        }
         
     }
     
