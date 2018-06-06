b0b345a51c7e780fa2858bf400847e115a247246
douglasBaumgratz <douglas.baumgratz@gmail.com>
2018-06-06 16:59:51 -0300

    sorteio

--- src/java/br/ufjf/dcc192/Evento.java
+++ src/java/br/ufjf/dcc192/Evento.java
@@ -2,12 +2,14 @@ package br.ufjf.dcc192;
 
 import java.sql.Timestamp;
 import java.util.Date;
+import java.util.List;
 
 /**
  *
  * @author Ramon Larivoir
  */
 public class Evento {
+
     long id;
     String titulo;
     double minimo;
@@ -56,8 +58,28 @@ public class Evento {
     public void setSorteio(Date sorteio) {
         this.sorteio = sorteio;
     }
+
     
-    public void sorteia() {
+    /*
+        Função sorteia retorna um sorteado por chamada de função, por isso contador foi 
+        criado fora da função e incrementado a cada vez que for feito um sorteio;    
+    */    
+    int contadorSorteio = 0; // contador começa com 0
+
+    public Participante sorteia(List<Participante> listaParticipantes, Participante participante) {
+        int totalParticipantes = listaParticipantes.size(); //total de participantes      
         
+        for (int i = 0; i < listaParticipantes.size(); i++) {
+            if (contadorSorteio < totalParticipantes) {
+                if (listaParticipantes.get(i) != participante && listaParticipantes.get(i) != null) {
+                    Participante sorteado = listaParticipantes.get(i);  //guarda sorteado
+                    listaParticipantes.remove(listaParticipantes.get(i)); //remove sorteado da lista
+                    listaParticipantes.remove(participante);
+                    contadorSorteio++;
+                    return sorteado;
+                }
+            }
+        }
+        return null;
     }
 }
